0c48fa8a273b8eea3341c935545db73e6339a9f4
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-11 12:43:37 -0300

    Creating new templates with HTML

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "FakePinterest"
+    return render_template("homepage.html")
+
+@app.route("/profile")
+def profile():
+    return "Perfil do Usuário"
 
 if __name__ == "__main__":
     app.run(debug=True)
