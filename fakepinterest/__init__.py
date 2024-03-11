dfea965742a46ae2c1755530c51db38d9054493d
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-11 17:08:48 -0300

    Creating database

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,6 +1,10 @@
 #Aqui é criado o app
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
+database = SQLAlchemy(app)
 
 from fakepinterest import routes
--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,1 +1,16 @@
 #Criar a estrutura do banco de dados
+from fakepinterest import database
+from datetime import datetime
+
+class User(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    password = database.Column(database.String, nullable=False)
+    photo = database.relationship("Photo", backref="user", lazy=True)
+
+class Photo(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    image = database.Column(database.String, default="default.png")
+    criation_date = database.Column(database.DateTime, nullable=False, default=datetime.utcnow())
+    id_user = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
