c860faab5d5a1cd249f8b6eebb93beb3db7e8505
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-12 13:43:22 -0300

    Creating Forms

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,1 +1,22 @@
 #criar os formulários do site
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import User
+class FormLogin():
+    emial = StringField("E-mail", validators=[DataRequired(), Email()])
+    password = PasswordField("Senha", validators=[DataRequired()])
+    confirm_button = SubmitField("Fazer Login")
+
+class FormCreateAccount():
+    email = StringField("E-mail", validators=[DataRequired, Email])
+    username = StringField("Nome de usuário", validators=[DataRequired])
+    password = PasswordField("Senha", validators=[DataRequired(), Length(6,20)])
+    confirm_password = PasswordField("Confirme a sua senha", validators=[DataRequired(), EqualTo("password")])
+    confirm_button = SubmitField("Cadastrar conta")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            return ValidationError("E-mail já cadastrado, faça o login para continuar")
+
--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -6,6 +6,7 @@ from flask_login import UserMixin
 @login_manager.user_loader
 def load_user(id_user):
     return User.query.get(int(id_user))
+
 class User(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
