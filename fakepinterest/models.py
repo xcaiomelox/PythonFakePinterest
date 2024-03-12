943c618e8a1c2928d5885d20f9d5fe8dd68ed6f8
Caio Cavalcante <caio.melo51@gmail.com>
2024-03-12 11:46:03 -0300

    Implementing login system

--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,8 +1,12 @@
 #Criar a estrutura do banco de dados
-from fakepinterest import database
+from fakepinterest import database, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(database.Model):
+@login_manager.user_loader
+def load_user(id_user):
+    return User.query.get(int(id_user))
+class User(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,11 +1,13 @@
 #Aqui ficarão as rotas, os links do nosso site
 from flask import render_template, url_for
 from fakepinterest import app
+from flask_login import login_required
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
 @app.route("/profile/<user>")
+@login_required
 def profile(user):
     return render_template("profile.html", user=user)
